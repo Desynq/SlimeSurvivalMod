2d894edfa6810bb20d6a8dab8345cbd8ea025827
Desynq <notdesynq@gmail.com>
2025-10-27 19:47:57 -0400

    Made it possible to get items currently on cooldown.

---
+++ src/main/java/io/github/desynq/slimesurvival/mixin/ItemCooldownsExt.java
@@ -0,0 +1,9 @@
+package io.github.desynq.slimesurvival.mixin;
+
+import net.minecraft.world.item.Item;
+
+import java.util.Set;
+
+public interface ItemCooldownsExt {
+    Set<Item> getItems();
+}
---
+++ src/main/java/io/github/desynq/slimesurvival/mixin/ItemCooldownsMixin.java
@@ -0,0 +1,22 @@
+package io.github.desynq.slimesurvival.mixin;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemCooldowns;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
+@Mixin(ItemCooldowns.class)
+public abstract class ItemCooldownsMixin implements ItemCooldownsExt {
+
+    @Shadow @Final private Map<Item, ?> cooldowns;
+
+    @Override
+    public Set<Item> getItems() {
+        return Collections.unmodifiableSet(cooldowns.keySet());
+    }
+}
